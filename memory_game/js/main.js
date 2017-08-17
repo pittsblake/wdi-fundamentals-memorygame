fbb8e51ffe811c7e4b77cc1e9989c152a7a9b98a
Blake Pitts <blakepitts@Blakes-MacBook-Air.local>
2017-08-17 07:39:23 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and Running!");
+var cardOne = "Queen";
+var cardTwo = "Queen";
+var cardThree = "King";
+var cardFour = "King";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
