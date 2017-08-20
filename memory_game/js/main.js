18919085675a4f814764fab486f13cd3d9a9d072
Blake Pitts <blakepitts@Blakes-MacBook-Air.local>
2017-08-20 14:44:33 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,27 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png"
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png"
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png"
+  }
+
+
+];
 var cardsInPlay = [];
 var checkForMatch = function(){
   
@@ -11,11 +34,13 @@ var checkForMatch = function(){
   };
 
 var flipCard = function (cardId) {
-    console.log("User flipped " + cards[cardId]);  
+    console.log("User flipped " + cards[cardId].rank +" " + cards[cardId].cardImage + " " + cards[cardId].suit);
+    
   
     checkForMatch();
+    cardsInPlay.push(cards[cardId].rank);
+    
 };
 
 flipCard(0);
 flipCard(2);
-cardsInPlay.push('flipCard');
