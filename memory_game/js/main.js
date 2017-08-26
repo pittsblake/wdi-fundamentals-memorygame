080f56daa70a68f151add924cdc4c0724153dfde
Blake Pitts <blakepitts@Blakes-MacBook-Air.local>
2017-08-26 12:58:08 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -25,6 +25,7 @@ var cards = [
 var cardsInPlay = [];
 var checkForMatch = function(){
   
+  this.setAttribute('src', cards[cardId].cardImage)
   if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
       console.log("You found a match!");
     } else {
@@ -33,7 +34,8 @@ var checkForMatch = function(){
   
   };
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id');
     console.log("User flipped " + cards[cardId].rank +" " + cards[cardId].cardImage + " " + cards[cardId].suit);
     
   
@@ -42,5 +44,15 @@ var flipCard = function (cardId) {
     
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+
+	for (var i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
